440b0af73e7f3f5e0228d0095c25e4d938c73cd5
oato.o <oat9002@hotmail.com>
2017-01-27 16:09:52 +0700

    Rearranged codes in model.py

--- model.py
+++ model.py
@@ -6,22 +6,28 @@ from weka.classifiers import Classifier
 from weka.filters import Filter
 from weka.core.classes import Random
 
-jvm.start()
 
-### load a dataset ###
-train_file = "./output/output_5EMO_2.csv"
-loader = Loader("weka.core.converters.CSVLoader")
-train_data = loader.load_file(train_file)
-train_data.class_is_last()
+### load a dataset CSV ###
+def load_dataset(path):
+    train_file = path
+    loader = Loader("weka.core.converters.CSVLoader")
+    train_data = loader.load_file(train_file)
+    train_data.class_is_last()
+    return train_data
 
 ###  Naive Bayes ###
-classifier = Classifier("weka.classifiers.bayes.NaiveBayes")
-# classifier.build_classifer(train_data)
-evaluation = Evaluation(train_data) 
-evaluation.crossvalidate_model(classifier, train_data, 10, Random(42)) 
-print(evaluation.summary())
-print(evaluation.confusion_matrix)
+def naivebay_classifier(data):
+    classifier = Classifier("weka.classifiers.bayes.NaiveBayes")
+    # classifier.build_classifer(data)
+    evaluation = Evaluation(data) 
+    evaluation.crossvalidate_model(classifier, data, 10, Random(42)) 
+    print(evaluation.summary())
+    print(evaluation.confusion_matrix)
+    return classifier
 
+jvm.start()
+train_data = load_dataset("./output/output_5EMO_2.csv")
+naivebay_classifier(train_data)
 jvm.stop()
 
 
