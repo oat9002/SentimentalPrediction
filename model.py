ec9da5688fdee2d229c6ec2b0c97b98e646ea73d
oato.o <oat9002@hotmail.com>
2017-01-27 15:51:07 +0700

    First test python-weka-wrapper

--- model.py
+++ model.py
@@ -1,25 +1,28 @@
 import os
 import weka.core.jvm as jvm
-from weka.core.dataset import Instances,Instance
 from weka.core.converters import Loader
 from weka.classifiers import Evaluation
 from weka.classifiers import Classifier
 from weka.filters import Filter
+from weka.core.classes import Random
 
+jvm.start()
 
-def main():
-    ### load a dataset ###
-    train_file_5EMO = "C:/Users/kingk/output_new5EMO_1.csv/"
-    loader = Loader("weka.core.converters.CSVLoader")
-    train5EMO_data = loader.load_file(train_file_5EMO)
-    train5EMO_data.class_is_last()
+### load a dataset ###
+train_file = "./output/output_5EMO_2.csv"
+loader = Loader("weka.core.converters.CSVLoader")
+train_data = loader.load_file(train_file)
+train_data.class_is_last()
 
+###  Naive Bayes ###
+classifier = Classifier("weka.classifiers.bayes.NaiveBayes")
+# classifier.build_classifer(train_data)
+evaluation = Evaluation(train_data) 
+evaluation.crossvalidate_model(classifier, train_data, 10, Random(42)) 
+print(evaluation.summary())
+print(evaluation.confusion_matrix)
 
-
-    # ###  Naive Bayes ###
-    # mlp = Classifier("weka.classifiers.bayes.Naive Bayes")
-    # mlp.build_classifer(train_file_5EMO)
-
+jvm.stop()
 
 
 
