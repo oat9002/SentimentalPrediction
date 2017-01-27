75be9dc4e3d268c91358492cd8fccdf462f76059
oato.o <oat9002@hotmail.com>
2017-01-27 16:24:24 +0700

    Created read and write model method(not test yet) and improved description

--- model.py
+++ model.py
@@ -5,9 +5,14 @@ from weka.classifiers import Evaluation
 from weka.classifiers import Classifier
 from weka.filters import Filter
 from weka.core.classes import Random
+import weka.core.serialization as serialization
 
 
-### load a dataset CSV ###
+"""
+Load dataset
+
+path: path where you want to load dataset
+"""
 def load_dataset(path):
     train_file = path
     loader = Loader("weka.core.converters.CSVLoader")
@@ -15,7 +20,11 @@ def load_dataset(path):
     train_data.class_is_last()
     return train_data
 
-###  Naive Bayes ###
+"""
+Classify with naive bayes algrorithm
+
+data: data you want to train
+"""
 def naivebay_classifier(data):
     classifier = Classifier("weka.classifiers.bayes.NaiveBayes")
     # classifier.build_classifer(data)
@@ -24,6 +33,25 @@ def naivebay_classifier(data):
     print(evaluation.summary())
     print(evaluation.confusion_matrix)
     return classifier
+
+
+"""
+Write a model to disk
+
+classifier: model that you want to write
+path: path where you want to write
+"""
+def save_model(classifier, path):
+    serialization.write(path, classifier)
+
+
+"""
+Read a model from disk
+
+path: path where you want to read
+"""
+def read_model(path):
+    return Classifier(jobject=serialization.read(path))
 
 jvm.start()
 train_data = load_dataset("./output/output_5EMO_2.csv")
