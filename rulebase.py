83fd0cb56251813ef8ceef55bd3f710d4498c6f6
oato.o <oat9002@hotmail.com>
2017-02-04 18:52:50 +0700

    Added many methods and fixed algorithm

--- rulebase.py
+++ rulebase.py
@@ -1,11 +1,82 @@
 import CSVExecutor
 import WordExecutor
+import random
 
 def check_emo_in_word(emo_list, word):
+    u = WordExecutor.createNgram(word=word, gram=1)
+    ub = WordExecutor.createNgram(word=word, gram=2)
+    ubt = WordExecutor.createNgram(word=word, gram=3)
     for item in emo_list:
-        if item in wd[0]:
-            return True
+        for w in u:
+            if item == w:
+                return True
+        for w in ub:
+            if item == w:
+                return True
+        for w in ubt:
+            if item == w:
+                return True
     return False
+
+def check_emo(data):
+    emo_list = data[1:]
+    count = 0
+    predicted_emo_list = []
+    for emo in emo_list:
+        if emo == '':
+            count += 1
+        else
+            predicted_emo_list.append(emo)
+    if count == 8:
+        rand_num = random.randint(1, 8)
+        data[rand_num] = get_emo(number=rand_num)
+        return data
+    elif count == 7:
+        return data
+    else:
+        rand_num = random.randint(0, len(predicted_emo_list) - 1)
+        emo = predicted_emo_list[rand_num]
+        data[]
+
+def get_emo(number):
+    if number == 1:
+        return 'joy'
+    elif number == 2:
+        return 'sadness'
+    elif number == 3:
+        return 'fear'
+    elif number == 4:
+        return 'angry'
+    elif number == 5:
+        return 'disgust'
+    elif number == 6:
+        return 'surprise'
+    elif number == 7:
+        return 'anticipation'
+    elif number == 8:
+        return 'acceptance'
+    else:
+        print('Enter a wrong number.')
+
+def revert_emo_to_number(emo):
+    if emo == 'joy':
+        return 1
+    elif emo == 'sadness':
+        return 2
+    elif emo == 'fear':
+        return 3
+    elif emo == 'angry':
+        return 4
+    elif emo == 'disgust':
+        return 5
+    elif emo == 'surprise':
+        return 6
+    elif emo == 'anticipation':
+        return 7
+    elif emo == 'acceptance':
+        return 8
+    else:
+        print('Enter a wrong emo.')
 
 keyword_list = CSVExecutor.read_csv_without_first_strange_char("Dataset/EMO_LIST.csv")
 for idx in range(0, len(keyword_list)):
@@ -70,5 +141,5 @@ for wd in words:
     else:
         temp.append('')
     list.append(temp)
-CSVExecutor.write_csv('output/labeled.csv', list)
+CSVExecutor.write_csv('output/labeled2.csv', list)
 # print(joy_list, sadness_list, fear_list, angry_list, disgust_list, surprise_list, anticipation_list, acceptance_list)
