0cc5ddb365e396f44402e10a11e4772e85966e0f
oato.o <oat9002@hotmail.com>
2017-02-06 00:02:31 +0700

    Now can check emoji

--- rulebase.py
+++ rulebase.py
@@ -1,21 +1,29 @@
 import CSVExecutor
 import WordExecutor
 import random
+import emoji
+
 
 def check_emo_in_word(emo_list, word):
     u = WordExecutor.createNgram(word=word, gram=1)
     ub = WordExecutor.createNgram(word=word, gram=2)
     ubt = WordExecutor.createNgram(word=word, gram=3)
     for item in emo_list:
-        for w in u:
-            if item == w:
-                return True
-        for w in ub:
-            if item == w:
-                return True
-        for w in ubt:
-            if item == w:
-                return True
+        if item == emoji.UNICODE_EMOJI:
+            for w in word:
+                if w == item:
+                    return True
+    for item in emo_list:
+        if item != emoji.UNICODE_EMOJI:
+            for w in u:
+                if item == w:
+                    return True
+            for w in ub:
+                if item == w:
+                    return True
+            for w in ubt:
+                if item == w:
+                    return True
     return False
 
 def check_emo(data):
