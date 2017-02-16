6f6d3c5b661778e3a0dd6edc47e10f0680fc2396
oato.o <oat9002@hotmail.com>
2017-02-16 15:46:48 +0700

    Renamed file

--- to_dataset.py
+++ to_dataset.py
@@ -2,7 +2,7 @@ import CSVExecutor
 import WordExecutor
 
 
-library = CSVExecutor.read_csv('./Dataset/5EMO.csv')
+library = CSVExecutor.read_csv('./Dataset/8EMO.csv')
 words = []
 for li in library:
     words.append(li[0])
@@ -26,11 +26,11 @@ ubt = WordExecutor.remove_strange_word_and_normalize(ubt)
 # print(ubt)
 freq = []
 for li in library:
-    li[0] = WordExecutor.frequency_occur_in_keyword(li[0], ub, 2)
+    li[0] = WordExecutor.frequency_occur_in_keyword(li[0], u, 1)
     freq.append(li)
 
-keywords = []
-keywords.append(u)
+# keywords = []
+# keywords.append(u)
 # print(CSVExecutor.to_dataset_format(freq, sorted(freq[0][0].keys())))
-# CSVExecutor.write_csv('output/output_5EMO_test.csv', CSVExecutor.to_dataset_format(freq, sorted(freq[0][0].keys())))
-CSVExecutor.write_csv('output/keyword.csv', keywords)
+CSVExecutor.write_csv('output/output_8EMO_2.csv', CSVExecutor.to_dataset_format(freq, sorted(freq[0][0].keys())))
+# CSVExecutor.write_csv('output/keyword8.csv', keywords)
