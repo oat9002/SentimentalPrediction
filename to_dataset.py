5b692dbc09d1adaf03a92f9ac4ec5c5d03dd21fd
oat9002 <oat9002@hotmail.com>
2017-02-23 17:54:16 +0700

    [bug] fix mutiprocessing

--- to_dataset.py
+++ to_dataset.py
@@ -1,7 +1,7 @@
 import CSVExecutor
 import WordExecutor
 import threading
-from multiprocessing import Process
+from multiprocessing import Process, Queue
 import gc
 import time
 
@@ -21,34 +21,11 @@ def divided_thread_len_end(size, total_thread):
         temp += divide
     return thread_len_end
 
-library = CSVExecutor.read_csv('./Dataset/8EMO_label.csv')
-words = []
-for li in library:
-    words.append(li[0])
-unigram = WordExecutor.create_ngram_from_list_bynltk(words, 1)
-bigram = WordExecutor.create_ngram_from_list_bynltk(words, 2)
-trigram = WordExecutor.create_ngram_from_list_bynltk(words, 3)
 
-u = WordExecutor.add_emoji_keyword(unigram)
-ub = WordExecutor.add_emoji_keyword(unigram + bigram)
-ubt = WordExecutor.add_emoji_keyword(unigram + bigram + trigram)
-
-# u = WordExecutor.remove_stop_word(u)
-u = WordExecutor.remove_strange_word_and_normalize(u)
-
-ub = WordExecutor.remove_stop_word(ub)
-ub = WordExecutor.remove_strange_word_and_normalize(ub)
-
-ubt = WordExecutor.remove_stop_word(ubt)
-ubt = WordExecutor.remove_strange_word_and_normalize(ubt)
-
-total_thread = divided_thread_len_end(size=len(library), total_thread=64)
-threads = []
-freq = []
-def caculate_freq_for_thread(start, end, dataset):
+def caculate_freq_for_thread(start, end, dataset, freq, keyword, gram):
     for idx in range(start, end):
-        dataset[idx][0] = WordExecutor.frequency_occur_in_keyword(dataset[idx][0], ub, 2)
-        freq.append(dataset[idx][0])
+        dataset[idx][0] = WordExecutor.frequency_occur_in_keyword(dataset[idx][0], keyword, gram)
+        freq.put(dataset[idx][0])
 
 def start_thread(thread_each_lap, threads):
     idx_start = 0
@@ -69,15 +46,45 @@ def start_thread(thread_each_lap, threads):
                 break
 
 
-for i in range(0, len(total_thread)):
-    if i != 0:
-        t = Process(target=caculate_freq_for_thread, args=(total_thread[i - 1], total_thread[i], library,))
-    else:
-        t = Process(target=caculate_freq_for_thread, args=(0, total_thread[i], library,))
-    threads.append(t)
 
-start_thread(thread_each_lap=4, threads=threads)
-print(time.time() - now)
+
+
+if __name__ == '__main__':    
+    freq = Queue()
+    library = CSVExecutor.read_csv('./Dataset/8EMO_label.csv')
+    words = []
+    for li in library:
+        words.append(li[0])
+    unigram = WordExecutor.create_ngram_from_list_bynltk(words, 1)
+    bigram = WordExecutor.create_ngram_from_list_bynltk(words, 2)
+    trigram = WordExecutor.create_ngram_from_list_bynltk(words, 3)
+
+    u = WordExecutor.add_emoji_keyword(unigram)
+    ub = WordExecutor.add_emoji_keyword(unigram + bigram)
+    ubt = WordExecutor.add_emoji_keyword(unigram + bigram + trigram)
+
+    # u = WordExecutor.remove_stop_word(u)
+    u = WordExecutor.remove_strange_word_and_normalize(u)
+
+    ub = WordExecutor.remove_stop_word(ub)
+    ub = WordExecutor.remove_strange_word_and_normalize(ub)
+
+    ubt = WordExecutor.remove_stop_word(ubt)
+    ubt = WordExecutor.remove_strange_word_and_normalize(ubt)
+
+    total_thread = divided_thread_len_end(size=len(library), total_thread=16)
+    threads = []
+    for i in range(0, len(total_thread)):
+        if i != 0:
+            t = Process(target=caculate_freq_for_thread, args=(total_thread[i - 1], total_thread[i], library, freq, u, 1))
+        else:
+            t = Process(target=caculate_freq_for_thread, args=(0, total_thread[i], library, freq, u, 1))
+        threads.append(t)
+    start_thread(thread_each_lap=4, threads=threads)
+    print(freq.get())
+    # CSVExecutor.write_csv('output/test_process.csv', CSVExecutor.to_dataset_format(freq, sorted(freq[0][0].keys())))
+    print(time.time() - now)
+
 # freq = []
 # for li in library:
 #     li[0] = WordExecutor.frequency_occur_in_keyword(li[0], u, 1)
