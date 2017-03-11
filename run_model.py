68eb7cd85f26d9f64a83589e271747fe7e75812c
oato.o <oat9002@hotmail.com>
2017-03-11 08:11:17 +0700

    Preapared for testing

--- run_model.py
+++ run_model.py
@@ -5,6 +5,7 @@ import weka.core.jvm as jvm
 from weka.classifiers import Evaluation
 from weka.classifiers import Classifier
 from weka.core.classes import Random
+from weka.filters import Filter
 import time
 #
 # keywords = CSVExecutor.read_csv('output/keyword.csv')
@@ -25,26 +26,48 @@ import time
 # jvm.stop()
 def run(dataset_path):
     start = time.time()
-    jvm.start()
+    
     ### load a dataset ###
     train_data = model.load_dataset_weka(dataset_path) #
-
+    to_nomial_class_filter = Filter(classname="weka.filters.unsupervised.attribute.NumericToNominal", options=["-R", "last"])
+    to_nomial_class_filter.inputformat(train_data)
+    
     ###  Naive Bayes ### Choose what you want
     classifier = Classifier("weka.classifiers.bayes.NaiveBayesMultinomial")
     # classifier = Classifier("weka.classifiers.bayes.NaiveBayes")
     # classifier.build_classifer(train_data)
-    evaluation = Evaluation(train_data)
-    evaluation.crossvalidate_model(classifier, train_data, 10, Random(42))
-    print(evaluation.summary())
-    print(evaluation.confusion_matrix)
+    evaluation = Evaluation(to_nomial_class_filter.filter(train_data))
+    evaluation.crossvalidate_model(classifier, to_nomial_class_filter.filter(train_data), 10, Random(42))
+    # print(evaluation.summary())
+    # print(evaluation.class_details())
+    # print(evaluation.matrix())
+    
 
 
         # ###  Naive Bayes ###
         # mlp = Classifier("weka.classifiers.bayes.Naive Bayes")
         # mlp.build_classifer(train_file_5EMO)
 
-    jvm.stop()
     print(time.time() - start)
 
 if __name__ == '__main__':
-    run('./output/For paper/train_1_with_emoticon')
+    jvm.start(max_heap_size="5g")
+    print('train_1_with_emoticon')
+    for i in range(0,10):
+        run('./output/For paper/train_1_with_emoticon.csv')
+    # print('\ntrain_1+2_with_emoticon')
+    # for i in range(0,10):
+    #     run('./output/For paper/train_1+2_with_emoticon.csv')
+    # print('\ntrain_2_with_emoticon')
+    # for i in range(0,10):
+    #     run('./output/For paper/train_2_with_emoticon.csv')
+    # print('\ntrain_1_without_emoticon')
+    # for i in range(0,10):
+    #     run('./output/For paper/train_1_without_emoticon.csv')
+    # print('\ntrain_1+2_without_emoticon')
+    # for i in range(0,10):
+    #     run('./output/For paper/train_1+2_without_emoticon.csv')
+    # print('\ntrain_2_without_emoticon')
+    # for i in range(0,10):
+    #     run('./output/For paper/train_2_without_emoticon.csv')
+    jvm.stop()
